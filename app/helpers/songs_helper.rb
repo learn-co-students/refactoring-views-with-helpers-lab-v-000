b89831fd5103485b615b0faa192bc11bee82023d
John Anthony Rivera <askinganthony@gmail.com>
2016-02-03 15:49:21 -0800

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,13 @@
 module SongsHelper
+	def display_artist(song)
+		(song.artist!=nil) ? link_to(song.artist_name, artist_path(song.artist)) : link_to("Add Artist", edit_song_path(song))
+	end
 
+	def display_song(song)
+		link_to(song.title, song_path(song))
+	end
+
+	def display_song_and_artist(song)
+		display_song(song)+" - "+display_artist(song)
+	end
 end
