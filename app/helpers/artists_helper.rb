7424b2c585ff586f09c918fc62ab3ffe47ef79c4
Karuna <imkaruna@gmail.com>
2016-03-20 21:21:01 -0400

    Done

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,9 @@
 module ArtistsHelper
+  def display_artist(name)
+    if name.artist.nil?
+      link_to 'Add Artist', edit_song_path(name)
+    else
+      link_to name.artist_name, artist_path(name.artist)
+    end
+  end
 end
