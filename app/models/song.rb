645a383d5dd2d158c9899f5daef7622b9b6aeaf5
agdavid <antonio.g.david@gmail.com>
2016-04-22 13:42:43 -0400

    Done. Code helper method and call in show/index views.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,13 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
 
+  #helper method following Law of Demeter/one dot rule
   def artist_name
+    self.artist.name
   end
 
+  #helper method following Law of Demeter/one dot rule
   def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
   end
 end
