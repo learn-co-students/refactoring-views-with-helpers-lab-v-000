df7327833ddf553ea357b2ea3c85a31d0f039f78
gormanjp <jpgorman8@gmail.com>
2016-04-28 16:13:33 -0400

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,9 @@
 module SongsHelper
-
+	def display_artist(song)
+		if song.artist_name == nil || song.artist_name == ''
+			link_to 'Add Artist', edit_song_path(song)
+		else
+			link_to "#{song.artist_name}", artist_path(song.artist)
+		end 
+	end
 end
