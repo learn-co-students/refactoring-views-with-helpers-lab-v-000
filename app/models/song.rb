f56ceef7c70096b05680e810806b89e05dce7b42
Gandhi Anderson <thegands@gmail.com>
2016-04-28 15:23:53 -0600

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,14 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    artist ? artist.name : nil
   end
 
   def artist_name=(name)
+    if artist
+      artist.name = name
+    else
+      self.artist = Artist.find_or_create_by(name: name)
+    end
   end
 end
