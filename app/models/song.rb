d3e02d0ecd85ea48bdfc155057dce755c52eef3c
jasonpaulso <hello@jasonpaulsouthwell.com>
2016-05-18 13:03:08 -0700

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,14 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    artist.name
   end
 
   def artist_name=(name)
+    self.artist = Artist.where('lower(name) = ?', name.downcase).first 
+    self.artist ||= Artist.create(:name => name)
   end
+
+    
+
 end
