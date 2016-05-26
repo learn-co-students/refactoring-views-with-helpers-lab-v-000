99215e72a1f542459d2a77f6e3fb1461c35ef44d
jenjiyi <jenny.yamada@gmail.com>
2016-05-26 11:44:46 -0700

    done

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,12 @@
 module ArtistsHelper
+  def display_artist(song)
+    # if song.artist.present?
+    #     link_to song.artist.name, artist_path(song.artist)
+    # else
+    #     link_to "Add Artist", edit_song_path(song)
+    # end
+    link_to_if(song.artist.nil?, "Add Artist", edit_song_path(song)) do
+     link_to(song.artist.name, artist_path(song.artist))
+   end
+  end
 end
