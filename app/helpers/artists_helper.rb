3b423343bddc9ec54125818ecb474995a25ffe0e
ndaniele <n.daniele@gmail.com>
2016-08-03 15:29:00 -0400

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -5,6 +5,8 @@ module ArtistsHelper
       link_to "Add Artist", edit_song_path(song)
     else
       link_to song.artist_name, artist_path(song.artist)
+
+      #link_to artist_path(song.artist), song.artist_name
     end
   end
   
