1cfb52e2bf9982709046b720157a6a23cbe9212d
jchu4483 <jchu4483@gmail.com>
2016-08-16 16:13:17 -0400

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,9 @@
 module SongsHelper
-
+  def display_artist(song)
+    if song.artist_name != nil
+      link_to song.artist.name, artist_path(song.artist)
+    elsif song.artist_name == nil
+      link_to "Add Artist", edit_song_path(song)
+    end
+  end
 end
