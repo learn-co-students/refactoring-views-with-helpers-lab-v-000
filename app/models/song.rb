1210dcdbfef0125ab823ee4f33079aa9324abb80
kriskanya <kriskanya@gmail.com>
2016-09-06 12:04:06 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,18 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
 
+  # After initialization, set default values
+  after_initialize :set_default_values
+
+  def set_default_values
+    self.artist ||= nil
+  end
+
   def artist_name
+    self.artist.name
   end
 
   def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
   end
 end
