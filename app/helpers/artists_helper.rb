cf59ecf5a4cbe2788677ea0251c8627ef6a03912
bshap93 <bshap93@gmail.com>
2016-09-18 19:31:29 -0500

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,9 @@
 module ArtistsHelper
+  def display_artist(song)
+    if song.artist
+      link_to song.artist_name, artist_path(song.artist)
+    else
+      link_to "Add Artist", [:edit, song]
+    end
+  end
 end
