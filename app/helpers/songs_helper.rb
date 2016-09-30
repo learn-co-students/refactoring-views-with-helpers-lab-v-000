b5ee2382dd17a616495273398a176739bc1f3c28
ed-karabinus <ejkarabin@gmail.com>
2016-09-29 22:18:34 -0500

    Refactoring Views with Helpers Lab

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,9 @@
 module SongsHelper
-
+  def display_artist(song)
+    if !song.artist || song.artist.name.nil?
+      return link_to "Add Artist", edit_song_path(song)
+    else
+      return link_to song.artist.name, artist_path(song.artist)
+    end
+  end
 end
