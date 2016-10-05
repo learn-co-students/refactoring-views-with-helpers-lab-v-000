41d6cb81b336009010e34e260c5d1678faaed28e
tylertaylor <vikingtyty@gmail.com>
2016-10-05 11:46:45 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,7 +1,4 @@
 class Artist < ActiveRecord::Base
   has_many :songs
 
-  def name=(name)
-
-  end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,7 +2,9 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
-    self.artist.name
+    if self.artist
+      self.artist.name
+    end
   end
 
   def artist_name=(name)
