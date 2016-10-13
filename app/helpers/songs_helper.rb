1b32e875b38e08f9572b4201b04d3e2891f6a93f
Lenn Hypolite <lenntriniman@hotmail.com>
2016-10-13 00:06:34 +0000

    4 more tests

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -2,7 +2,7 @@ module SongsHelper
 
   def display_artist(song)
      link_to_if(song.artist.nil?, "Add Artist", edit_song_path(song)) do
-       link_to song.title, song_path(song)
+       link_to song.artist.name, artist_path(song.artist)
      end
   end
 
