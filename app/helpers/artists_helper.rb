ba8fa24ffff85f3a0b11b367a0726fc4220a49be
johnsontaiwo <josseytee@gmail.com>
2016-11-11 11:55:23 -0500

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -3,6 +3,7 @@ module ArtistsHelper
   def display_artist(song)
     if !song.artist
       link_to "Add Artist", edit_song_path(song)
+      
     else
       link_to song.artist.name, artist_path(song.artist)
     end
