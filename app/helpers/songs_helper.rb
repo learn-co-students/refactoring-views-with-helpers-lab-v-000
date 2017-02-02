4221b7fa12fa07d99a66a987e8540d5a790cbaa0
Benjamin Cantlupe <bjcantlupe@gmail.com>
2017-02-02 15:45:27 +0000

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,10 @@
 module SongsHelper
-
+  def display_artist(song)
+    artist = song.artist
+    if !artist.nil? && !artist.name.empty?
+      link_to artist.name, artist
+    else
+      link_to "Add Artist", edit_song_path(song)
+    end
+  end
 end
