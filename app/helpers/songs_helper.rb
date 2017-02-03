5c3a8e863ecb07b7e18bc0449156e170eee5bdd4
WilliamMena <williammena1991@gmail.com>
2017-02-02 23:24:25 -0500

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,9 @@
 module SongsHelper
-
+  def display_artist(song)
+    if song.artist_name == nil || song.artist_name.empty?
+      link_to "Add Artist", edit_song_path(song)
+    else
+      link_to song.artist_name, song.artist
+    end
+  end
 end
