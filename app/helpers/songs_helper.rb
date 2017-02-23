76f90b18999cd4daa4c8393e2b1029654aa4a2c0
Jon <jon.fougner@yahoo.com>
2017-02-23 02:30:13 +0000

    pall all specs

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,12 @@
 module SongsHelper
 
+    def display_artist(song)
+      if song.artist_name
+          link_to song.artist_name, artist_path(song.artist)
+      else
+          # binding.pry
+          link_to "Add Artist", edit_song_path(song)
+      end
+    end
+
 end
