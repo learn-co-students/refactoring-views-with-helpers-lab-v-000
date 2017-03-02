684cb96fe20b36c851d36063c1bf406f0e95bf2f
Aleksandr <aleksandr.rogachev1994@gmail.com>
2017-03-02 11:46:31 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,13 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+  validates :title, presence: true
 
   def artist_name
+    self.artist.name if self.artist
   end
 
   def artist_name=(name)
+    artist = Artist.find_or_create_by(name: name)
+    self.artist = artist
   end
 end
