7483657c963cb74847ec60089f7216c22bd5f1a9
Kevin Adkins <kevinladkins@gmail.com>
2017-03-21 17:03:22 +0000

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,13 @@
 module ArtistsHelper
+
+
+def display_artist(song)
+  if song.artist
+    artist = Artist.find_by(name: song.artist.name)
+    link_to artist.name, artist_path(artist)
+  else
+    link_to "Add Artist", edit_song_path(song)
+  end
+end
+
 end
