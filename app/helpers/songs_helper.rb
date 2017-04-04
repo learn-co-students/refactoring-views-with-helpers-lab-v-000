f02fe52e30b69a0db66f42a33082990f2a78d8c9
Sean Clarke <xseanclarke@gmail.com>
2017-04-04 22:58:37 +0000

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,19 @@
 module SongsHelper
 
+  def display_artist(song)
+    if song.artist == nil
+      link_to "Add Artist", edit_song_path(song)
+    else
+      link_to song.artist_name, artist_path(song.artist)
+    end
+  end
+
+  def artist_name
+    artist.name
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
 end
