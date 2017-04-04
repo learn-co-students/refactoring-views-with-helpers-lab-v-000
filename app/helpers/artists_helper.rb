d990abe2bcceeffdb999ef97d5a39bc0296c638e
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-04-04 15:15:28 -0400

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,13 @@
 module ArtistsHelper
+    
+    def display_artist(song)
+        @artist = song.artist
+        if @artist && @artist.name
+            @link = link_to @artist.name, @artist
+        else
+            @link = link_to "Add Artist", edit_song_path(song)
+        end
+        
+        return @link
+    end
 end
