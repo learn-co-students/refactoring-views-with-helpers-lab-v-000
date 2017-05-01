bbbb1ebb51f55e8fd69bf753ff088934262384e2
M. Simon Borg <msimonborg@gmail.com>
2017-04-30 21:13:44 -0400

    done

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,10 @@
 module ArtistsHelper
+  def display_artist(song)
+    artist = song.artist
+    if artist
+      link_to artist.name, artist_path(artist)
+    else
+      link_to 'Add Artist', edit_song_path(song)
+    end
+  end
 end
