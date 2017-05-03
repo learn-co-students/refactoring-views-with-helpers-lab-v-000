2baa88028d797312c0ad8c92cae346bf1fcc3389
DanL12186 <danlrcn@hotmail.com>
2017-05-02 21:15:51 -0400

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,10 +1,5 @@
 class ArtistsController < ApplicationController
   before_action :set_artist, only: [:show, :edit, :update, :destroy]
-  def index
-  end
-
-  def show
-  end
 
   def new
     @artist = Artist.new
