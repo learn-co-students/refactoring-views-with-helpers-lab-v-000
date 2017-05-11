bac015d2c8e295057cb980b314d77c57cf11eb4e
anytimesoon <damien@delartigue.co.uk>
2017-05-11 21:21:54 +0100

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,13 @@
+require 'pry'
 class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+	  self.artist.name
   end
 
   def artist_name=(name)
+  	artist = Artist.find_or_create_by(name: name)
+  	artist.songs << self
   end
 end
