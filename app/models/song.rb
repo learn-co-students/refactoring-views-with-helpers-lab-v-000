5e61c96886cb7357f548e71e7766865d13463d05
Menachem Hornbacher <me@mendel.tech>
2017-06-11 21:05:00 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,15 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    self.artist.name
   end
 
   def artist_name=(name)
+    if self.artist.nil?
+      artist = Artist.find_by(name: name)
+      artist.nil? ? self.artist = Artist.create(name: name) : self.artist = artist
+    else
+      self.artist.update(name: name)
+    end
   end
 end
