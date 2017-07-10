5a2291308956af0529ebcc6662edfe414fc2e846
mjdele <mike.delehant@gmail.com>
2017-07-10 15:11:46 -0400

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,9 +1,3 @@
 module SongsHelper
-  def display_artist(song)
-    if song.artist
-      link_to song.artist.name, artist_path(song.artist)
-    else
-      link_to "Add Artist", edit_song_path(song)
-    end
-  end
+
 end
