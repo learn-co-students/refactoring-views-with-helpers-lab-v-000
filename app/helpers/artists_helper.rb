71a95ba172c503eeb52d10b3b4bbfdc6fe4a26f4
Saad <Dropheaven@live.com>
2017-07-18 07:11:48 -0400

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,11 @@
 module ArtistsHelper
+
+  def display_artist(song)
+    if song.artist == nil
+      link_to "Add Artist", "/songs/#{song.id}/edit"
+    else
+      link_to song.artist_name, artist_path(song.artist)
+    end
+  end
+
 end
