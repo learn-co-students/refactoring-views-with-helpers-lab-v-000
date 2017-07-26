9b952a513df4fb161b84007d40b49262e7f2de7a
Will-Sauer <will@heueso.com>
2017-07-26 13:03:10 -0700

    helper working

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,13 @@
 module SongsHelper
 
-end
+  def display_artist(song)
+    if song.artist
+    #If the artist name is not nil, return a link to the artist's show page
+      link_to(song.artist_name, artist_path(song.artist))
+    else
+    #If the artist name is nil, return a link to the song's edit page, with a link text of "Add Artist"
+      link_to("Add Artist", edit_song_path(song))
+    end
+  end
+
+end#eom
