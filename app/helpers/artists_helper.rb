19ef902e43d4f29f04c09f9337478028f082f314
Samson Yuwono <samson.yuwono@gmail.com>
2017-07-29 23:19:32 +0000

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,6 +1,11 @@
 module ArtistsHelper
 
   def display_artist(song)
+    if @artist.name != nil
+      link_to song.artist.name, @artist
+    else
+      link_to "Add Artist", edit_song_path(@song)
+    end
   end
 
 end
