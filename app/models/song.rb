603049ac5ceef9cdbd05d946aed35a7c8a41b376
Laamia Islam <laamia.islam@gmail.com>
2017-07-30 22:54:41 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,19 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    if self.artist
+      self.artist.name
+    end
   end
 
   def artist_name=(name)
+    if self.artist
+      self.artist.name = name
+    elsif Artist.find_by(name: name)
+      artist = Artist.find_by(name: name)
+      self.artist = artist
+    else
+      self.artist = Artist.new(name: name)
+    end
   end
 end
