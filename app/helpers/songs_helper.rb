2a189445ef7f0f24fa08b2334dfc255cbb0259be
Noah Summers <summers.noah@gmail.com>
2017-08-09 18:11:10 -0400

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,11 @@
 module SongsHelper
 
+  def display_artist(song)
+    if song.artist
+      render('artists/link', artist: song.artist)
+    else
+      link_to("Add Artist", edit_song_path(song))
+    end
+  end
+
 end
