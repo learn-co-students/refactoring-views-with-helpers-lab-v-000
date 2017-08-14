d024604919f61e87c2d75ca4f05cd1835ee6806a
Spencer Evans <spencevans@gmail.com>
2017-08-14 13:07:24 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,12 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    artist.name if self.artist
   end
 
   def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
   end
+
+  validates :title, presence: true
 end
