05864dc95e9dd12d9f7ec718ab18e5b01e8fcdd6
CLackrone <charisaanne@gmail.com>
2017-08-16 11:43:03 -0400

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,11 @@
 module SongsHelper
 
+  def display_artist(song)
+    if !song.artist
+      link_to 'Add Artist', edit_song_path(song)
+    else
+      link_to song.artist.name, song.artist
+    end
+  end
+
 end
