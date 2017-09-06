c61f40e3d15e8efd25ee15c00f4ae3cc711f97bd
gb23 <gregbenj@hotmail.com>
2017-09-06 12:21:46 -0700

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,9 +1,3 @@
 module SongsHelper
-    def display_artist
-        if self.artist.nil?
-            link_to "Add Artist", edit_song_path(self)
-        else
-            link_to artist_path(self.artist)
-        end
-    end
+
 end
