21e192d9a0ddc19cc57be9e008a2f6ee2d57e16c
Terry Blue <terryblue@comcast.net>
2017-09-15 23:12:08 +0000

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,11 @@
 module SongsHelper
 
+  def display_artist(song)
+    if song.artist
+      @result = link_to song.artist.name, artist_path(song.artist)
+    else
+      @result = link_to "Add Artist", edit_song_path(song)
+    end
+  end
+
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,10 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    self.artist.name
   end
 
   def artist_name=(name)
+    binding.pry
   end
 end
