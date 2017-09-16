c6365217cd90aa105bf935b891cb2c6d3c002d6d
Terry Blue <terryblue@comcast.net>
2017-09-16 00:21:03 +0000

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -2,9 +2,9 @@ module SongsHelper
 
   def display_artist(song)
     if song.artist
-      @result = link_to song.artist.name, artist_path(song.artist)
+      link_to song.artist.name, artist_path(song.artist)
     else
-      @result = link_to "Add Artist", edit_song_path(song)
+      link_to "Add Artist", edit_song_path(song)
     end
   end
 
