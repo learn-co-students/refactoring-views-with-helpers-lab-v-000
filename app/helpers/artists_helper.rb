3d8814eca49f54aaf87bb459130dded36a0edfed
sarahcyr <github email address>
2017-09-18 01:12:59 -0400

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,7 @@
 module ArtistsHelper
+
+  def display_artist(song)
+    song.artist.nil? ? link_to('Add Artist', edit_song_path(song))
+     : link_to(song.artist.name, song.artist)
+  end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,13 @@
+
 class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    self.artist.name
   end
 
   def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
   end
+   validates :title, presence: true
 end
