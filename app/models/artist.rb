4baf0ec9ca09f8241a8a577680946a80e029be62
Jake <jtb1137@gmail.com>
2017-10-13 17:07:19 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,16 @@
 class Artist < ActiveRecord::Base
   has_many :songs
+
+  def artist_name
+  	if self.artist != nil
+  		self.artist.name
+  	else
+  		return nil
+  	end
+  end
+
+  def artist_name=(name)
+  	artist = Artist.find_or_create_by(name: name)
+  	self.artist = artist
+  end
 end
