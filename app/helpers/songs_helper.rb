614a2421b81179f977ecd43c548769a41f5dc663
Nicholas D'Amico <nickalan82@icloud.com>
2017-10-14 13:41:28 -0400

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,13 @@
 module SongsHelper
 
+	def display_artist(song)
+		# binding.pry
+		if artist = song.artist
+			# Return link to Artist Show page
+			link_to "#{artist.name}", artist_path(artist)
+		else
+			# Return link to Songs edit page.
+			link_to "Add Artist", edit_song_path(song)
+		end
+	end
 end
