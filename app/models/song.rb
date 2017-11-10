37d032c796df0a183d45ed5c0e50e005e75061ee
charlie lee <ctlee9096@gmail.com>
2017-11-10 04:43:01 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,14 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    self.artist.name
   end
 
   def artist_name=(name)
+    if (self.artist.nil?)
+     self.artist = Artist.new(name)
+   else
+     self.artist.name = name
+   end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+@artist = Artist.create!(name: "Daft Punk")
+@grid = @artist.songs.create!(title: "The Grid")
