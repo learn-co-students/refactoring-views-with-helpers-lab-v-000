ecdff96cca557387de361446b84b33cbfae74298
charlie  lee <ctlee9096@gmail.com>
2017-11-11 01:14:03 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,5 @@
 class Song < ActiveRecord::Base
-  belongs_to :artist, inverse_of: :songs
-
+  belongs_to :artist
   def artist_name
     self.artist.name
   end
@@ -8,9 +7,4 @@ class Song < ActiveRecord::Base
   def artist_name=(name)
      self.artist = Artist.find_or_create_by(name: name)
   end
-
-  # private
-  # def name_params
-  #   params.require(:song).permit(:name)
-  # end
 end
