76b98bdf59dbbb75dd6d89da197b72eae7c5e996
Sophia H <shhubeny@gmail.com>
2017-11-13 19:08:59 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,13 @@
 class Song < ActiveRecord::Base
+
   belongs_to :artist
 
   def artist_name
+    artist.name if artist
   end
 
   def artist_name=(name)
+    artist = Artist.find_or_create_by(name: name)
+    self.artist=artist #can't set the name to this because you're setting the name to the instance of the Artist class, which won't match
   end
 end
