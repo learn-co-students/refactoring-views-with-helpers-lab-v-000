909389b131967b028c8f6919c00d493a4f2b023a
johanesteves <jsesteves91@gmail.com>
2017-11-17 00:07:01 -0500

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,27 @@
 module ArtistsHelper
+
+  def artist_name(song)
+    if song.artist
+      song.artist.name
+    else
+      "Add Artist"
+    end
+  end
+
+  def artist_link(song)
+    if song.artist
+      song_path(song.artist.id)
+    else
+      edit_song_path(song)
+    end
+  end
+
+  def display_artist(song)
+    if song.artist
+      link_to song.artist.name, song.artist
+    else
+      link_to "Add Artist", edit_song_path(song)
+    end
+  end
+
 end
