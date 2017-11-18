2b6f3dfc55f08bc81350e1a67e80dc5f456a52b1
Rony Boeck <ramg.9@live.com>
2017-11-18 18:37:21 +0000

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,14 @@
 module ArtistsHelper
+
+  def display_artist(song)
+    # If artist name is not nil
+    if song.artist
+      # Return link to song's artist show page
+      link_to song.artist.name, song.artist
+    else
+      # If nil, return link to edit page with "Add Artist" text.
+      link_to "Add Artist", edit_song_path(song)
+    end
+  end
+
 end
