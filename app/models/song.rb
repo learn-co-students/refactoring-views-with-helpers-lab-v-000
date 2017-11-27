ed89a67436c518f11494f03f5d77810b5b256eac
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-27 15:09:30 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,11 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    self.artist.name
   end
 
   def artist_name=(name)
+    #raise  Artist.find_or_create_by(name: name).inspect
+    self.artist = Artist.find_or_create_by(name: name)
   end
 end
