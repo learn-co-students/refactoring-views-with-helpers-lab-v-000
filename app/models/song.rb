297a0693384b33f139a05fe2e8f659ccadee4775
Igor Eskin <github email address>
2017-12-02 16:15:10 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,13 @@
 class Song < ActiveRecord::Base
+
   belongs_to :artist
 
   def artist_name
+    self.artist.name
   end
 
   def artist_name=(name)
+    artist = Artist.find_or_create_by(name: name)
+    self.update(artist: artist)
   end
 end
