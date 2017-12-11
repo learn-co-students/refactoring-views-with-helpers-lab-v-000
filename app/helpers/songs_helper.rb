24c3b215eac3182f65d0a60bcf4a463fa2c2af0d
EC2 Default User <ec2-user@ip-172-31-25-116.us-east-2.compute.internal>
2017-12-11 18:52:27 +0000

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,7 @@
 module SongsHelper
-
+    
+    def display_artist(song)
+        song.artist_name ? (link_to song.artist_name, artist_path(song.artist)) : (link_to "Add Artist", controller: "songs", action: "edit", id: song)
+    end
+ 
 end
