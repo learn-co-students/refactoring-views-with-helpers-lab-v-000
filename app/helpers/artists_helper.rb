6d5be699c3e7da235d8ec9df0e657ce0ab01d785
Sam Ojling <s.ojling@gmail.com>
2017-12-11 20:40:19 -0800

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,9 @@
 module ArtistsHelper
+  def display_artist(song)
+    if song.artist.nil?
+      link_to "Add Artist", edit_song_path(song)
+    else
+      link_to song.artist.name, artist_path(song.artist)
+    end
+  end
 end
