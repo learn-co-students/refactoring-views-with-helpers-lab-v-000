a8647c4680138e9feb006ce34cd3c98c07d45157
Ben <baschenbrenner@hotmail.com>
2017-12-14 17:48:49 +0000

    Done.

--- spec/views/songs/index.html.erb_spec.rb
+++ spec/views/songs/index.html.erb_spec.rb
@@ -5,7 +5,8 @@ RSpec.describe 'songs/index' do
     Artist.destroy_all
     Song.destroy_all
     @song = Song.create(title: "Bullet the Blue Sky", artist_name: "U2")
-    allow(view).to receive(:display_artist).and_return("They Might Be Giants")
+    allow(view).to receive(:display_artist2).and_return("They Might Be Giants")
+   
     assign(:songs, Song.all)
   end
 
