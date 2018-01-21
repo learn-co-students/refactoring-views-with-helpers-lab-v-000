8b46bb2597aae04eef0b941cb0793ad77641bf06
Robert Laws <roblaws@me.com>
2018-01-21 06:59:11 +0000

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,17 @@
 module SongsHelper
+  def link_to_artist(song)
+    if(song.artist)
+      link_to song.artist.name, artist_path(song.artist.id)
+    else
+      link_to "Add Artist", edit_song_path(song)
+    end
+  end
 
+  def link_to_song(song)
+    if(song.artist)
+      link_to song.title, song_path(song.id)
+    else
+      link_to "Add Artist", edit_song_path(song)
+    end
+  end
 end
