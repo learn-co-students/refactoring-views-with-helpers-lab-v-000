f19e17f391691535f6dd96c274990bf58fa5ee89
Donnadieu <donnadieu.86@gmail.com>
2018-02-04 12:27:29 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,14 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    self.artist.name unless self.artist == nil
   end
 
   def artist_name=(name)
+    self.artist = Artist.find_or_create_by(:name => name) unless name.empty?
+  end
+
+  def has_artist?
+    !!self.artist
   end
 end
