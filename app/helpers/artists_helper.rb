ee569a28f07a771e516c49f95982254f5a3eaa5f
EC2 Default User <ec2-user@ip-172-31-46-51.us-west-2.compute.internal>
2018-02-12 17:50:06 +0000

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,11 @@
 module ArtistsHelper
+    
+    def display_artist(song)
+        if song.artist != nil
+            @artist = song.artist
+           link_to @artist.name, artist_path(@artist)
+        else
+            link_to "Add Artist", edit_song_path(song)
+        end
+    end
 end
