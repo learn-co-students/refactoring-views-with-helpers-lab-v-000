56b243b06c6221bed75e9dad3e46161ee66436b7
Scott Wakeman <github email address>
2018-02-20 16:48:59 -0800

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,22 @@
 module ArtistsHelper
+  def display_artist(song)
+    #If an artist is already associated with the song, return a link to the artist's show page
+    if song.artist
+      #link_to artist_path, artist_path(artist_url)
+      #link_to 'artists/show'
+      link_to @song.artist_name, artist_path(song.artist)
+
+      #link_to artist_path, artist_path(:artist_params)
+
+    #If an artist is not associated('else'), return a link to the song's edit page,
+    #with a link text of "Add Artist"
+    else
+      link_to song_path, edit_song_path(song.artist.name)
+      #link_to @song, edit_song_path(@song)
+      #link_to 'songs/edit'
+
+    end
+
+
+  end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,13 +2,13 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
-    #self.artist.name if self.artist
-    self.try(:artist).try(:name)
+    self.artist.name if self.artist
+    #self.try(:artist).try(:name)
   end
 
   def artist_name=(name)
-    #self.artist = Artist.find_or_create_by(name: name)
-    artist = Artist.find_or_create_by(name: name)
-    self.artist = artist
+    self.artist = Artist.find_or_create_by(name: name)
+    #artist = Artist.find_or_create_by(name: name)
+    #self.artist = artist
   end
 end
