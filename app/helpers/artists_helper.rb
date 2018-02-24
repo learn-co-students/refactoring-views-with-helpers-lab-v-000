a80ecc874313351bd9e438a31bb0b2bc68f1d39e
daniellecroxton <danikrist@gmail.com>
2018-02-24 15:06:36 -0700

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,9 @@
 module ArtistsHelper
+  def display_artist(song)
+    if song.artist
+      link_to song.artist.name, song.artist
+    else
+      link_to "Add Artist", "/songs/#{song.id}/edit"
+    end
+  end
 end
