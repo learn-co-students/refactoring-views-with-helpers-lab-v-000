052d8dc69b063d349621842b9693f0903f47f7d7
Alex <github email address>
2018-03-07 14:05:54 -0500

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,12 @@
 module ArtistsHelper
+  def display_artist(song)
+#binding.pry
+      if song.artist #is valid
+            link_to song.artist.name, artist_path(song.artist)
+
+
+      else #if nil
+            link_to "Add Artist", edit_song_path(song)
+      end
+  end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,14 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    artist.name if artist
   end
 
+#lets not make duplicates
+#model methods are business logic
   def artist_name=(name)
+#binding.pry
+      self.artist = Artist.find_or_create_by(name: name)
+
   end
 end
