d346ae68fba2b39a389527a5e908ac6ecdd2acd4
Alex <github email address>
2018-03-07 14:26:42 -0500

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -3,8 +3,6 @@ module ArtistsHelper
 #binding.pry
       if song.artist #is valid
             link_to song.artist.name, artist_path(song.artist)
-
-
       else #if nil
             link_to "Add Artist", edit_song_path(song)
       end
