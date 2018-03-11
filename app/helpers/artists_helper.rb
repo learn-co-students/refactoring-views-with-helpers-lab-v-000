d6d40bd2bebcb00d833aef993d29e85511e3ea0a
Randall Robinson <keith_adams007@yahoo.com>
2018-03-11 19:07:11 +0000

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,12 @@
 module ArtistsHelper
+
+  def display_artist(song)
+    if song != nil
+      if song.artist != nil
+          '<a href="/artists/#{song.artist.id}">"#{song.artist.name}"</a>'
+      else
+          '<a href="/songs/#{song.id}/edit">Add Artist</a>'
+      end
+    end
+  end
 end
