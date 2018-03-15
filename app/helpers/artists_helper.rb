931a8fd4d3aeb624ee554b771a8bc49edee29002
joey michalina mariano <joey.michalina.mariano@gmail.com>
2018-03-15 19:02:46 +0000

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,10 +1,10 @@
 module ArtistsHelper
 
-  def display_artist(artist)
-    if artist != nil
-      link_to artist.name, artist_path
+  def display_artist(song)
+    if song.artist != nil
+      link_to song.artist.name, artist_path(song.artist)
     else
-      link_to "Edit Song.", edit_song_path
+      link_to "Add Artist", edit_song_path(song)
     end
   end
 
