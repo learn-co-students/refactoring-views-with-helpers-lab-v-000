89f32ca94376d3fa2684eb25efe927dfdbc9eeb7
Justin Acker <justin@justinacker.com>
2018-03-15 19:20:24 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,16 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    if self.artist
+      return self.artist.name
+    else
+      return "no artist"
+    end
   end
 
   def artist_name=(name)
+    artist = Artist.find_or_create_by(name: name)
+    self.artist = artist
+    self.save
   end
 end
