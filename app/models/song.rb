02f01918ace0d49582705f5d5cafd30dc6331274
Adam <github email address>
2018-03-20 14:53:09 -0600

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,15 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    unless self.artist == nil
+      self.artist.name
+    end
   end
 
   def artist_name=(name)
+    artist= Artist.find_or_create_by(name: name)
+    self.artist= artist
+    self.save
+    # binding.pry
   end
 end
