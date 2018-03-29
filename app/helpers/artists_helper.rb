e3801ad4993549a45b12f830b2b2ebdf93445be1
EC2 Default User <ec2-user@ip-172-31-8-108.ec2.internal>
2018-03-29 16:07:53 +0000

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,11 @@
 module ArtistsHelper
+    
+    def display_artist(artist_name)
+       if @song.artist_id
+          link_to artist_name, artist_path(@artist)
+       else
+           link_to "Add Artist", edit_song_path(@song)
+       end
+           
+    end
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,11 @@
 class Artist < ActiveRecord::Base
   has_many :songs
+  
+    def artist_name=(artist_name)
+        @artist_name = artist_name
+    end
+    
+    def artist_name
+        @artist_name
+    end
 end
