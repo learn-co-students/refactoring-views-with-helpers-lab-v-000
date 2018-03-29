3009238a2836cefe311936ba9c98e1ac6bc704b7
jacobsilver2 <jacobsilver2@mac.com>
2018-03-29 11:05:24 -0400

    uh

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,9 +1,12 @@
 module ArtistsHelper
+  
   def display_artist
     if song.artist
-      link_to artist_path(song.artist)
+      link_to song.artist.name, song.artist
     else
-      link_to 
+      link_to 'Add Artist', edit_song_path(song)
     end
   end
 end
+
+
