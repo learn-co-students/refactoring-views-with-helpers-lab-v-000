377420ffca1ff2cb34fe391be3617ce4f970dacd
EC2 Default User <ec2-user@ip-172-31-8-108.ec2.internal>
2018-03-29 17:21:28 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,11 +1,11 @@
 class Artist < ActiveRecord::Base
   has_many :songs
   
-    def artist_name=(artist_name)
-        @artist_name = artist_name
-    end
-    
-    def artist_name
-        @artist_name
-    end
+  def artist_name
+
+  end
+
+  def artist_name=(name)
+   
+  end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,10 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    @artist_name
   end
 
   def artist_name=(name)
+    @artist_name = name
   end
 end
