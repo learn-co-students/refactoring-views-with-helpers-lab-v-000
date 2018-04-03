8d23d649aab9cbdaa0f2162b8b4787cafe86b81a
Sean Clifford <sclifford6262@gmail.com>
2018-04-03 00:09:23 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,14 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    self.artist.name if self.artist
   end
 
   def artist_name=(name)
+    if name.blank?
+      nil
+    else
+      self.artist = Artist.find_or_create_by(name: name)
+    end
   end
 end
