bfc49ec8a35950d2d39d36d58844fe7be5b86ba6
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-04-10 18:32:57 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,14 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    if artist != nil
+      artist.name
+    end
   end
 
   def artist_name=(name)
+    if !name.blank?
+      self.artist = Artist.where(name: name).first_or_create
+    end
   end
 end
