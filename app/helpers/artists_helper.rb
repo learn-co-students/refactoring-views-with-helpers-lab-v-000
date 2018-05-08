8eebad40733b47867f94e881bc1eab25ce862c33
Sean Oughton <seanoughton@gmail.com>
2018-05-08 00:20:24 +0000

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,5 +1,10 @@
 module ArtistsHelper
   def display_artist(song)
-    link_to song.title, song
+    if song.artist
+      link_to song.title, song
+    else
+      link_to "Add Artist", edit_song_path(song)
+    end
+
   end
 end
