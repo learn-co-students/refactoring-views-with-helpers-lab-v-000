09ece29d8d9aaa91ff9aa2cebe1f7a92ca5f05fc
ecssiah <ecssiah@gmail.com>
2018-05-17 19:18:51 -0500

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,11 @@
 module SongsHelper
 
+  def display_artist(song)
+    if song.artist.nil? || song.artist.name.blank?
+      link_to "Add Artist", edit_song_path(song)
+    else
+      link_to song.artist.name, song.artist
+    end
+  end
+
 end
