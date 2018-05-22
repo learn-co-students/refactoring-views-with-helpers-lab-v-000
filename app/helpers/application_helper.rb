89204acb000dffb09f19e54b6a051c9576abee17
Aubrey Neimeier <neimeier.1@osu.edu>
2018-05-22 16:35:02 +0000

    Done.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def display_artist
+    if self.artist.present?
+      #display link to artist
+    else
+      #display link to edit the song page
+    end
+  end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,10 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    self.artist.name
   end
 
   def artist_name=(name)
+    self.artist.name = name
   end
 end
