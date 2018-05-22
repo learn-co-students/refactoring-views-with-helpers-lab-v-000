d12a01897426a71ad741e7265375d6afbd43331d
Aubrey Neimeier <neimeier.1@osu.edu>
2018-05-22 17:58:06 +0000

    Done.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,3 @@
 module ApplicationHelper
-  def display_artist
-    if self.artist.present?
-      #display link to artist
-      link_to artist_path(self), artist.name
-    else
-      #display link to edit the song page
-      link_to edit_song_path(self), "Edit Song"
-    end
-  end
+  
 end
--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,13 @@
 module SongsHelper
+  def display_artist(song)
+    if song.artist
+      #display link to artist
+      link_to song.artist_name, artist_path(song.artist)
+    else
+      #display link to edit the song page
+      link_to "Add Artist", edit_song_path(song)
+    end
+  end
+
 
 end
