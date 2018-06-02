97329c67ac774c36f26dfec003deef664097468b
Raiet Intraub <raietsaeed@gmail.com>
2018-06-02 23:30:16 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -5,5 +5,18 @@ class Song < ActiveRecord::Base
   end
 
   def artist_name=(name)
+    if name != ""
+      artist = Artist.find_or_create_by(name: name)
+      self.artist = artist
+    end
   end
 end
+
+# rspec spec/models/song_spec.rb
+
+# rspec spec/views/songs/index.html.erb_spec.rb
+# rspec spec/views/songs/show.html.erb_spec.rb
+
+# rspec spec/helpers/artists_helper_spec.rb
+
+# rspec spec/features/songs_spec.rb
