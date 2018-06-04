b76c76cf667c4eb54b5a78cd746fb88a35e18cc0
github username <github email address>
2018-06-04 18:05:43 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,13 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
 
+
   def artist_name
+    artist.name if artist
   end
 
   def artist_name=(name)
+    artist = Artist.find_or_create_by(name: name)
+    self.artist = artist
   end
 end
