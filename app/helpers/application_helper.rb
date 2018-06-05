e86c722a60fb84c5cff9c68699adff92820d4bb1
Amanda <amandarebecca89@gmail.com>
2018-06-05 10:56:12 -0400

    Done.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def display_artist(song)
+   if !song.artist.try(:name).blank? #if artist is already associated with the song, return to the artist's show page
+     link_to song.artist_name, artist_path(song.artist)
+   else
+     link_to "Add Artist", edit_song_path(song)
+   end
+ end
+
 end
