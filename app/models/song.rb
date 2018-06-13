30e2f27ca2c52eb4487ea773681217fae6067d8d
Kaitlin Kelly <kk504408@gmail.com>
2018-06-13 00:47:24 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,18 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    if self.artist != nil
+      self.artist.name
+    else
+      nil
+    end
   end
 
   def artist_name=(name)
+    @artist_name = name
+    @artist = Artist.find_or_create_by(name: name)
+    self.artist = @artist
   end
+
+
 end
