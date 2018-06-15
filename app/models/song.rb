b3400be72712d8f59824453674b04dd81c48bb24
Monica Tamaru Jones <mntamaru@gmail.com>
2018-06-15 09:51:37 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,12 @@
+require 'pry'
 class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    self.artist.name
   end
 
   def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
   end
 end
