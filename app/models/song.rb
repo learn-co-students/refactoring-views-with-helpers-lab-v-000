5a814833ce9b0f07ab2f032e72174f0c76c0f002
amckean12 <amckean@me.com>
2018-06-19 12:41:13 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -6,7 +6,7 @@ class Song < ActiveRecord::Base
   end
 
   def artist_name=(name)
-    a = Artist.find_or_create_by(name: name)
-    self.artist = a
+    artist = Artist.find_or_create_by(name: name)
+    self.artist = artist
   end
 end
