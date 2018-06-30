e828bdfebf41c3c485cec7e4f1bfaf1544403144
ryanwevans <ryanwevans@icloud.com>
2018-06-29 21:11:18 -0700

    refactor form for editing/creating songs out into SongsHelper

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,15 @@
 module SongsHelper
 
+  def song_form(song)
+    form_for song do |f|
+      f.label :title
+      f.text_field :title
+      
+      f.label :artist_name
+      f.text_field :artist_name
+
+      f.submit
+    end
+  end
+
 end
