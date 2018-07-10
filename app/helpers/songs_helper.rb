05ee3f16eb5c431524eda7a43f46527021e7cade
Alex Zhang <alexgzhang92@gmail.com>
2018-07-10 04:54:13 +0000

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,6 +1,6 @@
 module SongsHelper
   def display_artist(song)
-    if song.artist
+    if song.artist && song.artist.name != ""
       link_to song.artist.name, artist_path(song.artist)
     else
       link_to "Add Artist", edit_song_path(song)
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,7 +2,7 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
-    self.artist.name
+    self.artist.name if self.artist
   end
 
   def artist_name=(name)
