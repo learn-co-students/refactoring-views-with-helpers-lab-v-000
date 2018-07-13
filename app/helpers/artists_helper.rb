c99d306760546d29915bd547d4f4c56db7578184
Vanessa Toro <vanetoro84@gmail.com>
2018-07-13 14:40:25 +0000

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,10 +1,10 @@
 module ArtistsHelper
 
-  def display_artist
-   if @song.artist
-     link_to @song.artist.name, artist_path(@song)
+  def display_artist(song)
+   if song.artist
+     link_to song.artist.name, artist_path(song)
     else
-     link_to "Add Artist", edit_song_path(@song)
+     link_to "Add Artist", edit_song_path(song)
     end
 end
 
