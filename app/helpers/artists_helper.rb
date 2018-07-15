bb15cf3da4e00ceb163e241c49bd7a224fe4e578
Karl Amoguis <ktamoguis@gmail.com>
2018-07-15 06:59:17 +0000

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,14 @@
 module ArtistsHelper
+
+  def display_artist(song)
+    #binding.pry
+    if song.artist.nil?
+      #binding.pry
+      link_to("Add Artist", edit_song_path(song))
+    else
+      #binding.pry
+      link_to(song.artist.name, artist_path(song.artist))
+    end
+  end
+
 end
