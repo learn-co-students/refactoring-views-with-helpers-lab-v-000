b64cfd9a843742d56638ea19058528c81e429934
SamanthaMGarcia <samantha.marie.garcia@gmail.com>
2018-07-16 21:27:44 -0400

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -41,6 +41,10 @@ RSpec.configure do |config|
     # `true` in RSpec 4.
     mocks.verify_partial_doubles = true
   end
+  RSpec.configure do |config|
+  # Use color in STDOUT
+  config.color = true
+end
 
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
