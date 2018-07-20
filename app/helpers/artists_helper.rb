073db474a2cb244ec2102764635c89d592f78c9b
Jul Jen <juliajen.prog@gmail.com>
2018-07-20 05:20:08 +0000

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,11 @@
 module ArtistsHelper
+
+  def display_artist(song)
+    if song.artist
+      link_to song.artist.name, song.artist #If artist is associated with song, returns link to artist's show page
+    else
+      link_to "Add Artist", edit_song_path(song) #If an artist is not associated with the song (a.k.a. 'else'), returns to the song's edit page, "Add Artist"
+    end
+  end
+  
 end
