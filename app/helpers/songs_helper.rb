53c9fcf1ac4d10fc6c417456937e93949fd84360
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-07-30 05:07:17 +0000

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,7 +1,8 @@
 module SongsHelper
   def display_artist (song)
     if song.artist
-      display = "<a href='artists/#{song.artist_id}'>#{song.artist_name}</a>"
+      display = "<a href='../artists/#{song.artist_id}'>#{song.artist_name}</a>"
+      #display = "link_to #{song.artist_name}, song_path(#{song})"
     else
       display = "<a href='songs/#{song.id}/edit'>Add Artist</a>"
     end
