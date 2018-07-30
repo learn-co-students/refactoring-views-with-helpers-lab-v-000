6678d03c4c4386d355cfb0e4961e28ec74e4c632
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-07-30 04:59:11 +0000

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,10 @@
 module SongsHelper
-
+  def display_artist (song)
+    if song.artist
+      display = "<a href='artists/#{song.artist_id}'>#{song.artist_name}</a>"
+    else
+      display = "<a href='songs/#{song.id}/edit'>Add Artist</a>"
+    end
+    display.html_safe
+  end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,14 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    self.artist ? self.artist.name : nil
   end
 
   def artist_name=(name)
+    if self.artist
+      self.artist.name = name
+    else
+      self.artist = Artist.new(name)
+    end
   end
 end
