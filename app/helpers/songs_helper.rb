1422bc676226a7feb126e39b59dc83c73382a40a
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-07-31 16:39:58 +0000

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,12 +1,5 @@
 module SongsHelper
   
-    
-  def artist_name=(name)
-    self.artist = Artist.find_or_create_by(name: name)
-  end
   
-  def artist_name(name)
-    self.artist ? self.artist.name : nil
-  end
 
 end
