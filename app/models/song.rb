a078f396c57a962aece83e10e4dc123475fd4a17
Maverick Tesini <mavericktesini@gmail.com>
2018-08-01 22:22:51 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,7 +3,7 @@ class Song < ActiveRecord::Base
 
   def artist_name=(name)
     artist = Artist.find_or_create_by(name: name)
-    self.artist = artist
+    # self.artist = artist
   end
 
   def artist_name
