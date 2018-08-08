7d8fc04fcdeef09e3a2fd3f32341e06f93dab4a2
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-08 17:09:20 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,19 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    self.artist.name if artist
   end
 
-  def artist_name=(name)
+  def artist_name=(artist_name)
+    a= Artist.find_or_create_by(name: artist_name)
+    self.artist = a
   end
+
+
+  # song = Song.new(title: "Mad World")
+  # song.artist_name = "Tears for Fears"
+  # song.save
+  # expect(song.artist_name).to eq "Tears for Fears"
+
+
 end
