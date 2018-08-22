09ca37bc1794192c185a4dc4ad917a004855328d
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-08-22 04:28:47 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,11 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    self.artist.name if artist
   end
 
   def artist_name=(name)
+    artst = Artist.find_or_create_by(name: name)
+    self.artist = artst
   end
 end
