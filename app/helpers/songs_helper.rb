c6f003a11cbeb3cf1c61ec2c2b2524716f65299b
Courtney <admin@Courtneys-Macbook-Air-2.local>
2018-08-30 15:14:17 -0400

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,7 +1,7 @@
 module SongsHelper
 
   def display_artist(song)
-    if !!song.artist
+    if song.artist != nil
       link_to(song.artist.name, artist_path(song.artist))
     else
       link_to("Add Artist", edit_song_path(song))
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,10 +2,14 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
-    self.artist.name
   end
 
   def artist_name=(name)
-    self.artist = Artist.find_or_create_by(name: name)
+    if name != ""
+      self.artist = Artist.find_or_create_by(name: name)
+    else
+      self.artist = nil 
+    end
   end
+
 end
