71c35cf03b93544d72d7128b60ab03333a98ca5f
Nate Miller <natekmiller96@gmail.com>
2018-09-12 23:21:53 +0000

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,10 @@
 module ArtistsHelper
+  def display_artist(song)
+    Artist.all.each do |artist|
+      if artist.songs.include?(song)
+        return  link_to artist.name, artist_path(artist)
+      end
+    end
+    link_to "Add Artist", edit_song_path(song)
+  end
 end
