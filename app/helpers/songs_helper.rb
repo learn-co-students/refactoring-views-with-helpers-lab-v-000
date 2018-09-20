8724f0472a61b780c6c5889476b51f4c919be2a1
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-09-20 05:13:25 +0000

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -3,7 +3,7 @@ module SongsHelper
     if song.artist_name
       link_to song.artist_name, artist_path(song.artist)
     else
-      link_to 'Add Artist', edit_song(song)
+      link_to 'Add Artist', edit_song_path(song)
     end
   end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -8,6 +8,6 @@ class Song < ActiveRecord::Base
   end
 
   def artist_name=(name)
-    self.artist = Artist.find_by(name: name)
+      self.artist = Artist.find_or_create_by(name: name)
   end
 end
