bb5be8ebc19cee2270d595971e71b5a875ecac95
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-09-20 05:04:40 +0000

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,9 @@
 module SongsHelper
-
+  def display_artist(song)
+    if song.artist_name
+      link_to song.artist_name, artist_path(song.artist)
+    else
+      link_to 'Add Artist', edit_song(song)
+    end
+  end
 end
