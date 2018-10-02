22111864464d41f1263dbe96f13af94d502f121c
Brett Weisberg <weisberg.brett@gmail.com>
2018-10-02 10:34:37 +0000

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -4,7 +4,7 @@ module ArtistsHelper
     if song.artist == nil
       link_to 'Add Artist', edit_song_path(song)
     else
-      return song.artist
+       link_to song.artist.name, artist_path(song.artist)
     end
   end
 end
