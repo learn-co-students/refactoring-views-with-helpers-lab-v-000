d14e15dc359d0f3282f3071250e848baee1f03d2
pracniscate <lorena.giddens@outlook.com>
2018-10-02 23:15:01 -0400

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,11 @@
 module ArtistsHelper
+    def display_artist(song)
+        # If an artist is already associated with the song, return a link to the artist's show page
+        if song.artist.present?
+            link_to song.artist.name, artist_path(song.artist)
+        else
+            # If an artist is not associated with the song (a.k.a. 'else'), return a link to the song's edit page, with a link text of "Add Artist"
+            link_to "Add Artist", edit_song_path(song)
+        end
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,21 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160126235547) do
+ActiveRecord::Schema.define(version: 2016_01_26_235547) do
 
   create_table "artists", force: :cascade do |t|
-    t.string   "name"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "songs", force: :cascade do |t|
-    t.string   "title"
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "artist_id"
+    t.integer "artist_id"
+    t.index ["artist_id"], name: "index_songs_on_artist_id"
   end
-
-  add_index "songs", ["artist_id"], name: "index_songs_on_artist_id"
 
 end
