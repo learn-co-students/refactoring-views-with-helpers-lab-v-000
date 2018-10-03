3800ecb189016926f758c00e829eccac6876f8a7
mseaman6 <monicaameeti@gmail.com>
2018-10-02 21:11:38 -0400

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,11 +1,10 @@
 module ArtistsHelper
 
   def display_artist(song)
-    if artist_name
-      artist_path(song.artist)
+    if song.artist_name
+      link_to song.artist_name, artist_path(song.artist)
     else
-      edit_song_path(song)
-      #with a link text of "Add Artist"
+      link_to "Add Artist", edit_song_path(song)
     end
   end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,12 +1,11 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
-  validates :name, presence: true
 
   def artist_name
-    self.artist.name
+    self.artist.name unless self.artist.blank?
   end
 
   def artist_name=(name)
-    self.artist = Artist.find_or_create_by(name: name)
+    self.artist = Artist.find_or_create_by(name: name) unless name.blank?
   end
 end
