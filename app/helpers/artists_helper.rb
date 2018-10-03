1283f36e0669a98f5e8831076121902b9cac2aec
mseaman6 <monicaameeti@gmail.com>
2018-10-02 20:52:21 -0400

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,11 @@
 module ArtistsHelper
+
+  def display_artist(song)
+    if artist_name
+      artist_path(song.artist)
+    else
+      edit_song_path(song)
+      #with a link text of "Add Artist"
+    end
+  end
 end
