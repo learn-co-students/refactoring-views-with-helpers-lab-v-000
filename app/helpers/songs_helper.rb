5f01987cd200a5ae8687ffa9a2d9538cd8fec251
Christopher P. Dundon <chris.dundon@gmail.com>
2018-10-11 16:11:33 -0400

    lab complete

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,12 @@
 module SongsHelper
+	def display_artist(song)		
+		art_ = (song ? song.artist : nil)
 
+		if art_
+			link_to art_.name, artist_path(art_)
+		else
+			# binding.pry if song.id == 168
+			link_to 'Add Artist', edit_song_path(song)
+		end
+	end
 end
