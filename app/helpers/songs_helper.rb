291c7de7beb5c60d5b369a8ad0196a9dd4a9bb6c
Anu Khambete <anukhambete@gmail.com>
2018-10-18 17:53:15 +0000

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,6 +1,4 @@
 
 module SongsHelper
-  def artist_name
-    name = self.artist.name
-  end
+
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -6,5 +6,7 @@ class Song < ActiveRecord::Base
   end
 
   def artist_name=(name)
+    artist = Artist.find_or_create_by(name: name)
+    self.artist = artist
   end
 end
