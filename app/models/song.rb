fc8fc481ec9db07b3b64b04ef61c5dff09195c4d
Obafemi Fadairo <ofpupp@princeton.edu>
2018-10-27 15:07:18 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,20 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    return self.artist.name
   end
 
   def artist_name=(name)
+    this_artist = nil
+    Artist.all.each do |art|
+      if art.name == name
+        this_artist = art
+      end
+    end
+    if this_artist == nil
+      self.artist = Artist.create!(name: name)
+    else
+      self.artist = this_artist
+    end
   end
 end
