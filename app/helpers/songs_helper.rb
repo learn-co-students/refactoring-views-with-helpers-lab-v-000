5549b25441467bd71ca40cc50ae418134849178e
Kayla Cross <kac288@georgetown.edu>
2018-10-28 14:55:20 -0400

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,11 @@
 module SongsHelper
 
+  def display_artist(song)
+    if song.artist
+      link_to song.artist_name, artist_path(song.artist)
+    else
+      link_to "Add Artist", action: "edit", controller: "songs", id: song 
+    end
+  end
+
 end
