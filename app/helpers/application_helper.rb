a167f07d5c862054fc1e3137431fcfcba1c97a07
atxrenegade <harleighabel@gmail.com>
2018-12-01 22:55:06 -0600

    Done.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,3 @@
 module ApplicationHelper
-	def display_artist(song)
-		song.artist.nil? ? new_artist(song) : found_artist(song)
-  	end
-
-	def new_artist(song)
-		link_to 'Add Artist', edit_song_path(song)
-	end
-
-	def found_artist(song)
-		link_to song.artist_name, artist_path(song.artist)
-	end
+	
 end
