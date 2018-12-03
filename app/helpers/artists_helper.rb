7a0d210c78e4d912459bcfd045ea1ddbdc719207
amitnyc83 <amit_nyc@live.com>
2018-12-03 09:59:28 -0500

    displays artist

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,25 @@
 module ArtistsHelper
+
+  def display_artist(song)
+    if song.artist == nil
+      edit_song
+    else
+      artist_page
+    end
+  end
+
+
+
+
+  def edit_song(song)
+    link_to "Add Artist", edit_song_path(song)
+  end
+
+
+  def artist_page(song)
+    link_to song.artist_name, artist_path(song.artist)
+  end
+
+
+
 end
