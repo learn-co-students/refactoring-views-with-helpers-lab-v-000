dc866e1a2006b7349d793aa714155da2bbef27f2
Ludmilla Aires Negri <ludaires@gmail.com>
2018-12-03 14:46:26 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,15 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    self.artist.name if self.artist
   end
 
   def artist_name=(name)
+    if name != ""
+      self.artist = Artist.find_or_create_by(name: name)
+    else
+      self.artist = Artist.create(name: name)
+    end
+    self.artist.name
   end
 end
