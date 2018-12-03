21b513cf925ae030a00379d0edb86d5263680c85
amitnyc83 <amit_nyc@live.com>
2018-12-03 11:33:18 -0500

    all test passing

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,16 @@
 module SongsHelper
 
+  def display_artist(song)
+    song.artist.nil? ? add_artist_to_song(song) : edit_song_artist(song)
+  end
+
+
+  def add_artist_to_song(song)
+    link_to "Add Artist", edit_song_path(song)
+  end
+
+
+  def edit_song_artist(song)
+    link_to song.artist.name, artist_path(song.artist)
+  end
 end
