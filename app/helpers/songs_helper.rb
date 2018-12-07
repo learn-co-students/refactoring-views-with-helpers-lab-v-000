9929e791f40f89d987e002401641bc14051c66b0
github username <github email address>
2018-12-06 17:15:54 -0800

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,11 @@
 module SongsHelper
 
+  def artist_or_edit(song)
+    if song.artist_id
+      link_to song.artist.name, artist_path(song)
+    else
+      link_to "Add Artist", edit_song_path(song)
+    end
+  end
+
 end
