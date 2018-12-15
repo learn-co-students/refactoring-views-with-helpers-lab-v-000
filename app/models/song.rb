fc4d248432fc8e5b630a8e455979bae8f5beee27
Akshay Dhingra <akshay1dhingra@gmail.com>
2018-12-14 19:28:23 -0600

    almost done

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,13 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    if artist != nil
+      artist.name
+    end
   end
 
   def artist_name=(name)
+    artist_in_database = Artist.find_or_create_by(name: name)
+    Song.artist = artist_in_database
   end
 end
