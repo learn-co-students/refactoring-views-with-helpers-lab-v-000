571d15c74956fd444e8ce04b095cef8f8d9a0a97
Peter Kreppein <pmkreppein@gmail.com>
2018-12-22 01:15:32 +0000

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,10 @@
 module ArtistsHelper
+  def display_artist(song)
+   if song.artist != nil
+     return link_to song.artist.name, artist_path(song.artist)
+   else
+     return link_to "Add Artist", edit_song_path(song)
+   end
+ end
 end
+
