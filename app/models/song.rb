7ecd69fe85896d7f2462198e767d2007c1394051
Zachary Lehmann <zlehmann@gmail.com>
2019-01-30 22:22:56 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,16 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    self.artist.name
   end
 
   def artist_name=(name)
+    existing_artist = Artist.find_by(name: name)
+    if existing_artist
+      self.artist = existing_artist
+    else
+      self.artist = Artist.create(name: name)
+    end
   end
 end
+ 
