dbba48c010d1ce6676ee814b402b1b42142228cd
Nikki A <nikki.apostolakis@gmail.com>
2019-02-02 17:01:09 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,13 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    if self.artist
+      self.artist.name
+    end
   end
 
   def artist_name=(name)
+    artist = Artist.find_or_create_by(name: name)
+    self.artist  = artist
   end
 end
