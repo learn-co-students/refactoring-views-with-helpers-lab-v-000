e44ab4531746dfa7dfd3c46cbcf21c28a641bddd
marsbhoward <marsbhoward@gmial.com>
2019-02-05 19:00:49 -0800

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,9 @@
 module ArtistsHelper
-end
+    def display_artist(song)
+      if song.artist_name
+        link_to song.artist_name, artist_path(song.artist)
+      else
+        link_to "Add Artist", edit_song_path(song)
+      end
+    end
+  end
