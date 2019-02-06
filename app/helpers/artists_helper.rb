80933a414d9ec3b40071b973f8cb61e56eb91d12
Judy McGoldrick <jjeanius@verizon.net>
2019-02-06 02:15:05 +0000

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,11 @@
 module ArtistsHelper
+
+  def self.display_artist
+    if artist == ""
+      redirect_to edit_song_path
+    else
+      redirect_to artist_path
+    end
+  end
+
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,10 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    @artist_name = @song.artist.name
   end
 
   def artist_name=(name)
+    @artist_name = name
   end
 end
