0626c438d8886314b4dbe2bb0619edb5fb7d9d3f
Melissa Real <melissa.real@hotmail.com>
2019-02-07 18:50:50 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,8 +1,10 @@
 class ArtistsController < ApplicationController
   def index
+    @artist = Artist.all
   end
 
   def show
+    @artist = Artist.find(params[:id])
   end
 
   def new
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,15 @@
+require 'pry'
+
 class SongsController < ApplicationController
   def index
+  
     @songs = Song.all
+
   end
 
   def show
     @song = Song.find(params[:id])
+
   end
 
   def new
@@ -50,4 +55,3 @@ class SongsController < ApplicationController
     params.require(:song).permit(:title, :artist_name)
   end
 end
-
