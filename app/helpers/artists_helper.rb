e97202fdd2be0881b2d00d59261cc33539c77b58
Ayana Bando <ayanaflax@yahoo.com>
2019-02-12 20:04:23 +0000

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,4 +1,4 @@
-1module ArtistsHelper
+module ArtistsHelper
   def display_artist(song)
     if song.artist == nil
       link_to "Add Artist", edit_song_path(song)
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,10 +2,10 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
-    @artist = Artist.find_by(params[:id])
+    self.artist.name if self.artist
   end
 
   def artist_name=(name)
-    
+    self.artist = Artist.find_or_create_by(name: name)
   end
 end
