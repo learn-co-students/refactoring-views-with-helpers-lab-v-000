4a440aee226cb7b020f56bb0eaafe8781995e08d
harleyharl <harleymteden@gmail.com>
2019-02-13 12:32:20 -0800

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,11 @@
 module SongsHelper
 
+  def link_to_song_artist(song)
+    if song.artist
+      link_to song.artist.name, artist_path(song.artist)
+    else
+      link_to "Add Artist", edit_song_path(song)
+    end
+  end
+
 end
