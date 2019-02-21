e74b14682bfa80d5a28f5b32680bd9a322c60210
Jonathan Chan <johnkchan252@gmail.com>
2019-02-21 03:27:13 +0000

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,6 +1,6 @@
 module ArtistsHelper
   def display_artist(song)
-    if song.artist == nil
+    if song.artist.nil?
       link_to("Add Artist", edit_song_path(song))
     else
       link_to(song.artist.name, artist_path(song.artist))
