37e01c2b5af7a85536c1e1bc280fa5626a05f794
Erin Green <erin.y.green@gmail.com>
2019-02-24 20:15:10 +0000

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,10 +1,10 @@
 module SongsHelper
 
   def display_artist(song)
-    if song.artist != nil 
-      redirect_to artist_path(song.artist.id)
-    else 
+    if song.artist == nil 
       link_to "Add Artist", song_path(song.id)
+    else 
+        redirect_to artist_path(song.artist.id)
     end 
   end
 
