5d3e9496c61bf4bbce9e775067cad55ee2626b9b
Steve Albonico <steve@albonico.net>
2019-02-27 09:51:23 -0800

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,10 @@
 module SongsHelper
 
+def display_artist
+	if self.artist != nil
+		<p><a href="/artists/#{self.artist.id}">Link</a></p>
+end
+
+
+
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,12 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+  	self.artist.name
   end
 
   def artist_name=(name)
+  
+    self.artist = Artist.create_or_find_by(name: name)
+   
   end
 end
