167d28da9659ff77f7ec0c1f6db784815a848fe2
Rebecca <rtong@buffalo.edu>
2019-02-27 17:01:19 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,6 @@
+require 'pry'
 class Artist < ActiveRecord::Base
   has_many :songs
+
+
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,21 @@
+require 'pry'
 class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    if self.artist
+      return self.artist.name
+    end
   end
 
   def artist_name=(name)
+    @artist = Artist.find_by(name: name)
+    if @artist == nil
+      self.artist = Artist.create(name: name)
+    else
+      self.artist = @artist
+    end
+    self.save
   end
+
 end
