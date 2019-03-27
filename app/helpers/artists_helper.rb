52ea3f8a70533f3ac79391088a5af4316f93763b
ricecake88 <vocalise@gmail.com>
2019-03-26 22:37:35 -0700

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -3,7 +3,7 @@ module ArtistsHelper
         if !song.artist.nil?
             link_to song.artist.name, artist_path(song.artist)
         else
-            link_to "Add Artist", edit_artist_path(song.artist)
+            link_to "Add Artist", edit_song_path(song)
         end
     end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -6,6 +6,7 @@ class Song < ActiveRecord::Base
   end
 
   def artist_name=(name)
-    artist.name = name
+    @artist = Artist.find_or_create_by(:name => name)
+    self.artist = @artist
   end
 end
