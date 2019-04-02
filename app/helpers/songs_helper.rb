1f3437debd66d442f3233eba3e2d42ce03807f8e
brennanmrose <brennan.m.rose@gmail.com>
2019-04-01 21:32:14 -0400

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,11 +1,3 @@
 module SongsHelper
 
-	def display_artist(song)
-		if song.artist.present?
-			link_to song.artist_name, artist_path(song.artist)
-		else
-			link_to 'Add an Artist', edit_song_path(song)
-		end
-	end
-
 end
