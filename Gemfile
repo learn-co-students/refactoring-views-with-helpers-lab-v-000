06e27cfe22c7f199b03185b2a85e1c8d20a64286
Leslie Stevens <flymaven@protonmail.com>
2019-04-07 10:26:33 -0500

    build helper

--- Gemfile
+++ Gemfile
@@ -11,9 +11,9 @@ group :development, :test do
   gem 'pry'
   gem 'capybara'
   gem 'rspec-rails'
+  gem 'pry-rails'
 end
 
 group :development do
   gem 'web-console', '~> 2.0'
 end
-
--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,11 @@
 module SongsHelper
 
+  def display_artist(artist)
+    if self.artist == artist
+      link_to artist.name, artist_path(artist)
+    else
+      link_to "Add Artist", edit_artist_path(path)
+    end
+  end
+
 end
