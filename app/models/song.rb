58fb4ee8369ae945e0fed0d444f18ee53e12b4be
sarawall1187 <sarawall1187@gmail.com>
2019-04-26 12:14:41 -0700

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,15 +1,15 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
 
+  def artist_name=(name)
+    artist = Artist.find_or_create_by(:name => name)
+    self.artist = artist
+  end
+  
   def artist_name
-    if @song.artist != nil
-     @song.artist.name
+    if self.artist != nil
+     self.artist.name
     end
   end
 
-  def artist_name=(name)
-    if name != nil
-      @song.build_artist(:name => name)
-    end
-  end
 end
