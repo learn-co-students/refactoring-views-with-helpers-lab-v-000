5500ddb23a8520244a5de16d0e12566e6b0db45a
Kristin Ortiz <itskristin.ortiz@gmail.com>
2019-05-26 20:56:08 -0700

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -4,7 +4,7 @@ module ArtistsHelper
     if song.artist
       link_to song.artist.name, artist_path(song.artist)
     else
-      link_to"add Artist", edit_song_path(song)
+      link_to"Add Artist", edit_song_path(song)
     end
   end
 
