2456398e575fabb48eee168fdd4aa42b36991486
Sushi Chanrai <sushi@pathworks.co>
2019-07-19 16:48:06 +0200

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,12 @@
 module ArtistsHelper
+  def display_artist(song)
+    if song.artist.present?
+      link_to song.artist.name, artist_path(song.artist)
+    #If an artist is not associated with the song (a.k.a. 'else'),
+    else
+      link_to "Add Artist", edit_song_path(song)
+    #return a link to the song's edit page, with a link text of "Add Artist"
+    end
+  end
+
 end
