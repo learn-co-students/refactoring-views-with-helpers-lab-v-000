3a9c389944ec3901a4a321d5105d75fde2587a88
Richard-Burd <qalbalassadalasfour@gmail.com>
2019-10-15 08:09:24 -0700

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,5 @@
 module SongsHelper
-
+# NOTE: you could put the:
+# def display_artist(song)
+# method here or in the artists_helper
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,11 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    artist.name
   end
 
   def artist_name=(name)
+    artist_name = Artist.find_or_create_by(name: name)
+    self.artist = artist_name
   end
 end
