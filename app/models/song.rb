2e9ec1c6298b672da592ed55c5010b9cbbb50465
MisterSenor <nathan.visak@gmail.com>
2019-11-21 11:45:11 -0600

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,12 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
 
-  def artist_name
+  def artist_name=(artist_name)
+    artist_name = Artist.find_by_name(artist_name)
   end
 
-  def artist_name=(name)
+  def artist_name
+    @artist_name
   end
+
 end
