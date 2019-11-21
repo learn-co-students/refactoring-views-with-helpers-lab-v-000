dedfa37fd2a93f2e3311cd089bec0b712daef46a
MisterSenor <nathan.visak@gmail.com>
2019-11-21 13:12:33 -0600

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,13 +2,13 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name=(artist_name)
-    artist_name = Artist.find_by_name(artist_name)
+    self.artist = Artist.find_or_create_by(name: artist_name)
+    # find the existing Artist with the given name
+    # OR create a new Artist with the given name
   end
 
   def artist_name
-    @artist_name
+    self.artist ? self.artist.name : nil
   end
-
-  binding.pry
 
 end
