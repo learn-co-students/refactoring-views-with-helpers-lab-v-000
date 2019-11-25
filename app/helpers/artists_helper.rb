f9be86e82d85434a1d27d474ea4b95e3996166b3
Jamie Berrier <berrier.jamie@gmail.com>
2019-11-25 19:14:29 +0000

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,12 @@
 module ArtistsHelper
+
+  def display_artist(song)
+    # If an artist is already associated with the song, return a link to the artist's show page
+    if song.artist
+      link_to song.artist_name, song.artist
+    else # return a link to the song's edit page, with a link text of "Add Artist"
+      link_to "Add Artist", edit_song_path(song)
+    end
+  end
+
 end
