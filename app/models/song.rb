4444f6eaa575a2f092dc46cbfa2fb1e2a9e3d3a0
Zain <zain@halai.us>
2020-01-16 15:17:17 -0600

    Update song.rb

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,11 +2,10 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
-    artist.name if artist
+      self.try(:artist).try(:name)
   end
 
   def artist_name=(name)
-    artist = Artist.find_or_create_by(name: name)
-    self.artist = artist
+      self.artist = Artist.find_or_create_by(name: name)
   end
 end
