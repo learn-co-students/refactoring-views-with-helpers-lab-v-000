b3e832f7a6ce9694704b1772eb915aa4348740f9
Sheena Bonilla <sheena.myb@gmail.com>
2020-05-16 23:22:31 +0000

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,9 +1,9 @@
 module ArtistsHelper
-  def display_artist(artist)
-    if !artist.nil?
-      link_to "<%= artist.name %>", artist_path(artist)
+  def display_artist(song)
+    if song.artist_id
+      link_to song.artist.name, artist_path(song.artist_id)
     else #not associated with a song
-      link_to "Add Artist", song_path(artist)
+      link_to "Add Artist", edit_song_path(song)
     end
   end
 end
