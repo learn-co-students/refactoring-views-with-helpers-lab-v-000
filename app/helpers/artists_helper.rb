181041d37b1215c40c2962c1ac096ad2d9bb014b
dejarvzal <diannejarvisj@gmail.com>
2020-08-14 17:03:36 -0700

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,13 @@
 module ArtistsHelper
+
+    def display_artist
+        if artist_name.empty?
+            redirect_to artist_path(@artist) 
+        else
+            redirect_to artist_path
+        end
+    end
+
+#If an artist is already associated with the song, return a link to the artist's show page  else
+# If an artist is not associated with the song, return a link to the song's edit page, with a link text of "Add Artist"
 end
