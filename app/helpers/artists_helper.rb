aef7597e8e541578a65e66a59eec040d9c68c360
lrh2daizzo <herrera.lrh@gmail.com>
2021-05-06 18:42:23 -0500

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,8 +1,8 @@
 module ArtistsHelper
     def display_artist(song)
-        if song.artist 
+        if song.artist
             link_to song.artist_name, artist_path(song.artist)
-        else
+        else 
             link_to 'Add Artist', edit_song_path(song)
         end
     end
